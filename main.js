6d25bf7b500cdbfcdd737f650a77d479dcdcc183
Rukky <abrukevwe@gmail.com>
2023-09-08 22:10:09 +0100

    done

--- main.js
+++ main.js
@@ -12,17 +12,17 @@ const updateDateTime = () => {
 
     const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
-    const hours = today.getUTCHours().toString().padStart(2, '0');
-    const minutes = today.getUTCMinutes().toString().padStart(2, '0');
-     const seconds = today.getUTCSeconds().toString().padStart(2, '0');
+    // const hours = today.getUTCHours().toString().padStart(2, '0');
+    // const minutes = today.getUTCMinutes().toString().padStart(2, '0');
+    //  const seconds = today.getUTCSeconds().toString().padStart(2, '0');
 
-     const milliseconds = ((hours * 3600) + (minutes * 60) + seconds) * 1000;
+    //  const milliseconds = ((hours * 3600) + (minutes * 60) + seconds) * 1000;
 
-     currentUTCTimeElement.textContent = `${milliseconds}`;
+     
 
 
-    //  const totalMilliseconds = today.getUTCHours() * (1 * 3600000) + today.getUTCMinutes() * 60000 + today.getUTCSeconds() * 1000 + today.getUTCMilliseconds();
-
+     const totalMilliseconds = today.getUTCHours() * (1 * 3600000) + today.getUTCMinutes() * 60000 + today.getUTCSeconds() * 1000 + today.getUTCMilliseconds();
+     currentUTCTimeElement.textContent = `${totalMilliseconds}`;
     
 
 
