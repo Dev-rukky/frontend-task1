f8eebec3ddb91d492b92adc13aa2fbc4434be3e3
Rukky <abrukevwe@gmail.com>
2023-09-09 05:23:42 +0100

    done

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
-
-
 const updateDateTime = () => {
     const today = new Date();
+
+
     const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
     const currentDayElement = document.querySelector(`[data-testid="currentDayOfTheWeek"]`);
+
+
     currentDayElement.textContent = daysOfTheWeek[today.getUTCDay()];
 
-    
 
     const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
-    // const hours = today.getUTCHours().toString().padStart(2, '0');
-    // const minutes = today.getUTCMinutes().toString().padStart(2, '0');
-    //  const seconds = today.getUTCSeconds().toString().padStart(2, '0');
+    const utcMilliSeconds = Date.now();
 
-    //  const milliseconds = ((hours * 3600) + (minutes * 60) + seconds) * 1000;
-
-     
-
-
-     const totalMilliseconds = today.getUTCHours() * (1 * 3600000) + today.getUTCMinutes() * 60000 + today.getUTCSeconds() * 1000 + today.getUTCMilliseconds();
-     currentUTCTimeElement.textContent = `${totalMilliseconds}`;
-    
-
-
+     currentUTCTimeElement.textContent = utcMilliSeconds;
 } 
 
 updateDateTime();
